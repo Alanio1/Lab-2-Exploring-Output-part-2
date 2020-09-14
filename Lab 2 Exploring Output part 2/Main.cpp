54c330a6770e99ee1463400dab066b56822fc3bf
Alanio Reese <reese51@tritons.iowacentral.edu>
2020-09-14 12:25:56 -0500

    Add project files.

---
+++ Lab 2 Exploring Output part 2/Main.cpp
@@ -0,0 +1,78 @@
+//**
+//This section is for the second half of the assignment
+//**
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string p = "*";
+string e = "   ";
+
+
+int main()
+
+{
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << e;//**
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << "*\n";
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+	cout << p;
+	cout << e;
+}
